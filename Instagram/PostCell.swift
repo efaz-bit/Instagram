cec57be7555d38f356db04b28c1464f48a626ed4
efaz-bit <18efaz.ahmed18@gmail.com>
2021-10-15 00:18:39 -0400

    first commit

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Efaz Ahmed on 10/14/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
